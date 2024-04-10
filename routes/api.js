206c75ec3511c06c91bb05c0d19c265527042d59
mylkner <mylofaulkner27@gmail.com>
2024-04-10 07:36:36 +0200

    added get to /api/convert

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,41 @@
-'use strict';
+"use strict";
 
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const expect = require("chai").expect;
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 module.exports = function (app) {
-  
-  let convertHandler = new ConvertHandler();
+    let convertHandler = new ConvertHandler();
 
+    app.get("/api/convert", (req, res) => {
+        const input = req.query.input;
+
+        const num = convertHandler.getNum(input);
+        const unit = convertHandler.getUnit(input);
+        const unitSpelledOut = convertHandler.spellOutUnit(unit);
+        const returnUnit = convertHandler.getReturnUnit(unit);
+        const returnUnitSpelledOut = convertHandler.spellOutUnit(returnUnit);
+        const returnNum = convertHandler.convert(num, unit);
+        const string = convertHandler.getString(
+            num,
+            unitSpelledOut,
+            returnNum,
+            returnUnitSpelledOut
+        );
+
+        if (num === "invalid number" && returnUnit === "invalid unit") {
+            res.json("invalid number and unit");
+        } else if (num === "invalid number") {
+            res.json("invalid number");
+        } else if (returnUnit === "invalid unit") {
+            res.json("invalid unit");
+        } else {
+            res.json({
+                initNum: Number(num),
+                initUnit: unit,
+                returnNum: Number(returnNum),
+                returnUnit: returnUnit,
+                string: string,
+            });
+        }
+    });
 };
